87791bc66235c9fab805cfc9a91d7fb182b09ff4
barf <barf>
2006-02-02 14:42:47 +0000

    Install in $(targetprefix) too, because of sillyness in the library reduction, that expects to find shared libs there

--- make/flashroot-neutrino.mk
+++ make/flashroot-neutrino.mk
@@ -1,3 +1,7 @@
+# Why install in $(targetprefix)? Because the library reduction
+# expects to find shared libraries there :-\ (Yes, it should be
+# fixed there.)
+
 $(flashprefix)/root-neutrino-cramfs-p \
 $(flashprefix)/root-neutrino-squashfs-p \
 $(flashprefix)/root-neutrino-jffs2-p: \
@@ -5,7 +9,8 @@ $(flashprefix)/root-neutrino-%-p: \
 $(flashprefix)/root-% $(appsdir)/tuxbox/neutrino/config.status
 	rm -rf $@
 	cp -rd $< $@
-	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install prefix=$@
+	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install
+	$(MAKE) -C $(appsdir)/tuxbox/neutrino install prefix=$@
 	$(MAKE) -C $(appsdir)/dvb/zapit install prefix=$@
 	cp $(appsdir)/tuxbox/enigma/data/fonts/bluebold.ttf $@/share/fonts
 	cp $(appsdir)/tuxbox/enigma/data/fonts/bluehigh.ttf $@/share/fonts
