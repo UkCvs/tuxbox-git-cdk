56c7a1e875e613bef272f1f87afdf942d46536ac
barf <barf>
2006-02-04 06:37:50 +0000

    Added customization. If the user has a customization, run it INSTEAD of the default action.

--- make/version.mk
+++ make/version.mk
@@ -1,16 +1,28 @@
+# Create the /.version file
+
+# If the user has a customization, run it INSTEAD of the default action.
+
 version: $(targetprefix)/.version
 
 flash-version: $(flashprefix)/root/.version
 
 $(flashprefix)/root/.version: $(flashprefix)/root
-	echo "version=0200`date +%Y%m%d%H%M`" 	> $@
-	echo "creator=`id -un`" 		>> $@
-	echo "imagename=newmake-image" 		>> $@
-	echo "homepage=http://www.tuxbox.org" 	>> $@
+	if [ -x flash-version-local.sh ] ; then \
+		sh flash-version-local.sh $(flashprefix) $(buildprefix); \
+	else \
+		echo "version=0200`date +%Y%m%d%H%M`" 	>  $@;	\
+		echo "creator=`id -un`" 		>> $@;	\
+		echo "imagename=newmake-image" 		>> $@;	\
+		echo "homepage=http://www.tuxbox.org" 	>> $@;	\
+	fi
 
 $(targetprefix)/.version: $(targetprefix)
-	echo "version=0200`date +%Y%m%d%H%M`" 	> $@
-	echo "creator=`id -un`" 		>> $@
-	echo "imagename=newmake-yadd" 		>> $@
-	echo "homepage=http://www.tuxbox.org" 	>> $@
+	if [ -x version-local.sh ] ; then \
+		sh version-local.sh $(targetprefix) $(buildprefix); \
+	else \
+		echo "version=0200`date +%Y%m%d%H%M`" 	 > $@;	\
+		echo "creator=`id -un`" 		>> $@;	\
+		echo "imagename=newmake-yadd" 		>> $@;	\
+		echo "homepage=http://www.tuxbox.org" 	>> $@;	\
+	fi
 
