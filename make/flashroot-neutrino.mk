f34483ff58eb605ec4902148872e28609b153be9
barf <barf>
2006-02-05 16:59:50 +0000

    Installation of neutrino and enigma for images is now more efficient

--- make/flashroot-neutrino.mk
+++ make/flashroot-neutrino.mk
@@ -1,21 +1,11 @@
-# Why install in $(targetprefix)? Because the library reduction
-# expects to find shared libraries there :-\ (Yes, it should be
-# fixed there.)
-
 $(flashprefix)/root-neutrino-cramfs-p \
 $(flashprefix)/root-neutrino-squashfs-p \
 $(flashprefix)/root-neutrino-jffs2-p: \
 $(flashprefix)/root-neutrino-%-p: \
-$(flashprefix)/root-% $(flashprefix)/root $(appsdir)/tuxbox/neutrino/config.status
+$(flashprefix)/root-% $(flashprefix)/root $(flashprefix)/root-neutrino
 	rm -rf $@
 	cp -rd $(flashprefix)/root $@
+	cp -rd $(flashprefix)/root-neutrino/* $@
 	cp -rd $</* $@
-	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install
-	$(MAKE) -C $(appsdir)/tuxbox/neutrino install prefix=$@
-	$(MAKE) -C $(appsdir)/dvb/zapit install prefix=$@
-	cp $(appsdir)/tuxbox/enigma/data/fonts/bluebold.ttf $@/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/bluehigh.ttf $@/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/pakenham.ttf $@/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/unmrs.pfa    $@/share/fonts
 	$(MAKE) $@/lib/ld.so.1
 	@TUXBOX_CUSTOMIZE@
--- make/neutrino.mk
+++ make/neutrino.mk
@@ -5,3 +5,23 @@ $(appsdir)/tuxbox/neutrino/config.status: bootstrap libid3tag libmad libvorbisid
 
 neutrino: $(appsdir)/tuxbox/neutrino/config.status
 	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install
+
+if TARGETRULESET_FLASH
+flash-neutrino: $(flashprefix)/root-neutrino
+
+# Why install in $(targetprefix)? Because the library reduction
+# expects to find shared libraries there :-\ (Yes, it should be fixed
+# there.)
+
+$(flashprefix)/root-neutrino: $(appsdir)/tuxbox/neutrino/config.status
+	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install
+	$(MAKE) -C $(appsdir)/tuxbox/neutrino install prefix=$@
+	$(MAKE) -C $(appsdir)/dvb/zapit install prefix=$@
+	cp $(appsdir)/tuxbox/enigma/data/fonts/bluebold.ttf $@/share/fonts
+	cp $(appsdir)/tuxbox/enigma/data/fonts/bluehigh.ttf $@/share/fonts
+	cp $(appsdir)/tuxbox/enigma/data/fonts/pakenham.ttf $@/share/fonts
+	cp $(appsdir)/tuxbox/enigma/data/fonts/unmrs.pfa    $@/share/fonts
+	touch $@
+	@TUXBOX_CUSTOMIZE@
+
+endif
