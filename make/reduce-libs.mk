f7adbfce2bcb11f7a4af210e99d52abf175bef79
barf <barf>
2006-02-22 16:50:44 +0000

    Explicitly copy liblufs-ftpfs.so.2.0.0, mklibs do not suffice

--- make/reduce-libs.mk
+++ make/reduce-libs.mk
@@ -17,6 +17,11 @@ $(flashprefix)/root-enigma-jffs2/lib/ld.so.1: \
 		`find $</lib/ -name "libnss_*"` \
 		`find $</lib/ -name "*.so" -type f` \
 		`find $</sbin/ -path "*sbin/?*"`
+	if [ -e $(flashprefix)/root/lib/liblufs-ftpfs.so.2.0.0 ]; then \
+		cp $(flashprefix)/root/lib/liblufs-ftpfs.so.2.0.0 $</lib/liblufs-ftpfs.so.2.0.0 ; \
+		ln -sf liblufs-ftpfs.so.2.0.0 $</lib/liblufs-ftpfs.so.2 ; \
+		ln -sf liblufs-ftpfs.so.2.0.0 $</lib/liblufs-ftpfs.so ; \
+	fi
 	$(target)-strip --remove-section=.comment --remove-section=.note \
 		`find $</bin/ -path "*bin/?*"` \
 		`find $</sbin/ -path "*sbin/?*"` 2>/dev/null || /bin/true
