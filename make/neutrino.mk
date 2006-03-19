3e7caf9786f37ef2f3ca8d206878a6573e1459c2
barf <barf>
2006-03-19 16:21:29 +0000

    Install some additional fonts with Neutrino, since plugins may need them without caring to install them. Kludge really. Problem reported by saruman.

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -5,6 +5,7 @@ $(appsdir)/tuxbox/neutrino/config.status: bootstrap libid3tag libmad libvorbisid
 
 neutrino: $(appsdir)/tuxbox/neutrino/config.status
 	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install
+	$(MAKE) neutrino-additional-fonts
 
 if TARGETRULESET_FLASH
 flash-neutrino: $(flashprefix)/root-neutrino
@@ -12,11 +13,16 @@ flash-neutrino: $(flashprefix)/root-neutrino
 $(flashprefix)/root-neutrino: $(appsdir)/tuxbox/neutrino/config.status
 	$(MAKE) -C $(appsdir)/tuxbox/neutrino all install prefix=$@
 	$(MAKE) -C $(appsdir)/dvb/zapit install prefix=$@
-	cp $(appsdir)/tuxbox/enigma/data/fonts/bluebold.ttf $@/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/bluehigh.ttf $@/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/pakenham.ttf $@/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/unmrs.pfa    $@/share/fonts
+	$(MAKE) neutrino-additional-fonts targetprefix=$@
 	touch $@
 	@TUXBOX_CUSTOMIZE@
 
 endif
+
+# This is really an ugly kludge. Neutrino and the plugins should
+# install the fonts they need in their own Makefiles.
+neutrino-additional-fonts:
+	cp $(appsdir)/tuxbox/enigma/data/fonts/bluebold.ttf $(targetprefix)/share/fonts
+	cp $(appsdir)/tuxbox/enigma/data/fonts/bluehigh.ttf $(targetprefix)/share/fonts
+	cp $(appsdir)/tuxbox/enigma/data/fonts/pakenham.ttf $(targetprefix)/share/fonts
+	cp $(appsdir)/tuxbox/enigma/data/fonts/unmrs.pfa    $(targetprefix)/share/fonts
