d7e2d7da849f05226f1f5335e01ca8f5203091ad
barf <barf>
2006-03-19 16:45:08 +0000

    Cleanup: no target named apps

--- make/phony.mk
+++ make/phony.mk
@@ -1,7 +1,7 @@
 if TARGETRULESET_FLASH
-.PHONY: core libs root apps boot devel java copy lib compress \
+.PHONY: core libs root boot devel java copy lib compress \
 	depsclean mostlyclean clean distclean \
 	flash-clean flash-semiclean flash-developerclean flash-mostlyclean
 else
-.PHONY: core libs root apps boot devel java depsclean mostlyclean clean distclean
+.PHONY: core libs root boot devel java depsclean mostlyclean clean distclean
 endif
