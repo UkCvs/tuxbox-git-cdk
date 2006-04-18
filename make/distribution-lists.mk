434eec7ef91600259eaaed5040b817d78d70de96
barf <barf>
2006-04-18 16:34:19 +0000

    New target update.list.

--- make/distribution-lists.mk
+++ make/distribution-lists.mk
@@ -42,3 +42,18 @@ allimages.list: $(flashprefix)/allimages.list
 $(flashprefix)/allimages.list: $(flashprefix)/img.list $(flashprefix)/squashfs.list $(flashprefix)/cramfs.list
 	cat $^ > $@
 	@TUXBOX_CUSTOMIZE@
+
+# Experimental target
+update.list: $(flashprefix)/update.list
+
+$(flashprefix)/update.list: $(shell $(customizationsdir)/ls-distributionfiles $(flashprefix) $(buildprefix) 2>/dev/null ||  echo $(flashprefix)/*.img*x $(flashprefix)/root-*.*fs )
+	@echo $^
+	rm -f $@
+	cd $(flashprefix) && \
+	for f in $^ ; do \
+	echo $(updatehttpprefix)`basename $$f` \
+		`md5sum -b <$$f |sed -e s/\*-//` \
+		`grep version $(flashprefix)/root/.version |sed -e s/version=//` \
+		`basename $$f` >> $@; \
+	done
+	@TUXBOX_CUSTOMIZE@
