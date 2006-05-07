fd6b0603d9742168359d068f4a952270a548a3a1
barf <barf>
2006-05-07 11:45:10 +0000

    Updated to take recent changes into account. Flashdirectories not precious any more. Use .DELETE_ON_ERROR.

--- make/precious.mk
+++ make/precious.mk
@@ -1,20 +1,21 @@
 ################################################################ 
 # This .PRECIOUS-bizarreness is there to tell Make that these files are not
-# just "intermediate products", but are not to be deleted.  For the
-# directories, these are not really valuable, however, make tries to
-# delete them with rm, and this fails, generating error messages that
-# may be ugly and/or confusing.
+# just "intermediate products", but are not to be deleted.
 
 .PRECIOUS: \
 $(flashprefix)/neutrino-cramfs.img% $(flashprefix)/enigma-cramfs.img% \
 $(flashprefix)/neutrino-squashfs.img% $(flashprefix)/enigma-squashfs.img% \
 $(flashprefix)/neutrino-jffs2.img% $(flashprefix)/enigma-jffs2.img% \
-$(flashprefix)/root-%-jffs2  $(flashprefix)/root-% \
-$(flashprefix)/root-neutrino-% $(flashprefix)/root-enigma-% \
-$(flashprefix)/root-neutrino-%-p $(flashprefix)/root-enigma-%-p \
-$(flashprefix)/vmlinuz-% \
 $(flashprefix)/var-%.jffs2 \
 $(flashprefix)/root-%.jffs2 \
 $(flashprefix)/root-%.cramfs $(flashprefix)/root-%.squashfs \
-%/lib/ld.so.1 \
-$(flashprefix)/root/lib/tuxbox/plugins/%.cfg
+%/lib/ld.so.1
+
+
+# If the actions of a target return nonzero exit status, delete the target.
+# Reference: make manual "5.5 Errors in Commands", 
+# http://www.gnu.org/software/make/manual/make.html#Errors
+# Requires patched make to work with directories.
+# See http://savannah.gnu.org/bugs/?func=detailitem&item_id=16372
+
+.DELETE_ON_ERROR:
