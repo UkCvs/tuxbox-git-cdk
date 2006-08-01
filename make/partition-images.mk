d60a099920604a2260e6db31e611dfca742ebfdf
the_moon <the_moon>
2006-08-01 05:32:45 +0000

    checkin forgotten partition-images

--- make/partition-images.mk
+++ make/partition-images.mk
@@ -1,7 +1,9 @@
 #### Targets for building $partition-$gui.$fstype
 
 ####### var-$gui.jffs2
-$(flashprefix)/var-neutrino.jffs2 $(flashprefix)/var-enigma.jffs2: \
+$(flashprefix)/var-radiobox.jffs2 \
+$(flashprefix)/var-neutrino.jffs2 \
+$(flashprefix)/var-enigma.jffs2: \
 $(flashprefix)/var-%.jffs2: $(flashprefix)/var-% $(MKJFFS2)
 	@if [ "$(MKJFFS2)" = "/bin/false" ] ; then \
 		echo "FATAL ERROR: No mkjffs2 or mkfs.jffs2 available"; \
@@ -9,20 +11,27 @@ $(flashprefix)/var-%.jffs2: $(flashprefix)/var-% $(MKJFFS2)
 	fi
 	$(MKJFFS2) -b -e 131072 -p -r $< -o $@
 
+
 ####### root-$gui.$fstype
-$(flashprefix)/root-neutrino.cramfs $(flashprefix)/root-enigma.cramfs: \
+$(flashprefix)/root-radiobox.cramfs \
+$(flashprefix)/root-neutrino.cramfs \
+$(flashprefix)/root-enigma.cramfs: \
 $(flashprefix)/root-%.cramfs: $(flashprefix)/root-%-cramfs $(MKCRAMFS)
 	$(MKCRAMFS) -p -n "0106`date +%Y%m%d%H%M`" $< $@
 	@TUXBOX_CUSTOMIZE@
 
-$(flashprefix)/root-neutrino.squashfs $(flashprefix)/root-enigma.squashfs: \
+$(flashprefix)/root-radiobox.squashfs \
+$(flashprefix)/root-neutrino.squashfs \
+$(flashprefix)/root-enigma.squashfs: \
 $(flashprefix)/root-%.squashfs: $(flashprefix)/root-%-squashfs $(MKSQUASHFS)
 	rm -f $@
 	$(MKSQUASHFS) $< $@ -be
 	chmod 644 $@
 	@TUXBOX_CUSTOMIZE@
-
-$(flashprefix)/root-neutrino.jffs2 $(flashprefix)/root-enigma.jffs2 $(flashprefix)/root-lcars.jffs2 $(flashprefix)/root-null.jffs2: \
+	
+$(flashprefix)/root-radiobox.jffs2 \
+$(flashprefix)/root-neutrino.jffs2 \
+$(flashprefix)/root-enigma.jffs2 $(flashprefix)/root-lcars.jffs2 $(flashprefix)/root-null.jffs2: \
 $(flashprefix)/root-%.jffs2: $(flashprefix)/root-%-jffs2 $(MKJFFS2)
 	@if [ "$(MKJFFS2)" = "/bin/false" ] ; then \
 		echo "FATAL ERROR: No mkjffs2 or mkfs.jffs2 available"; \
