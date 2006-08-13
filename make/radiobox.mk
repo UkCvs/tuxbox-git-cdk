c705aa47d3acfec7ac2618465bcfe1b995c29d7d
the_moon <the_moon>
2006-08-13 02:22:48 +0000

    moved radiobox related parts to rbx Makefiles

--- make/radiobox.mk
+++ make/radiobox.mk
@@ -1,6 +1,6 @@
 # tuxbox/radiobox
 
-$(appsdir)/tuxbox/radiobox/config.status: bootstrap libboost libcurl libfreetype libmad libid3tag libvorbisidec libtuxbox misc_libs tuxbox_libs misc_tools lufs
+$(appsdir)/tuxbox/radiobox/config.status: bootstrap libboost libcurl libfreetype libmad libid3tag libvorbisidec libtuxbox misc_libs tuxbox_libs misc_tools lufs lirc
 	cd $(appsdir)/tuxbox/radiobox && $(CONFIGURE)
 
 radiobox: $(appsdir)/tuxbox/radiobox/config.status
@@ -11,23 +11,8 @@ flash-radiobox: $(flashprefix)/root-radiobox
 
 $(flashprefix)/root-radiobox: $(appsdir)/tuxbox/radiobox/config.status
 	$(MAKE) -C $(appsdir)/tuxbox/radiobox all install prefix=$@
-	cp -r $(appsdir)/tuxbox/radiobox/root/* $@
-	$(MAKE) radiobox-additional-fonts targetprefix=$@
 	touch $@
 	@TUXBOX_CUSTOMIZE@
 
 endif
-
-# This is really an ugly kludge. Neutrino and the plugins should
-# install the fonts they need in their own Makefiles.
-radiobox-additional-fonts:
-	mkdir -p $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/bluebold.ttf $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/bluehigh.ttf $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/pakenham.ttf $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/enigma/data/fonts/unmrs.pfa    $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/neutrino/data/fonts/*.ttf      $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/neutrino/data/fonts/*.gz       $(targetprefix)/share/fonts
-	cp $(appsdir)/tuxbox/neutrino/data/fonts/*.pcf      $(targetprefix)/share/fonts
-
 
