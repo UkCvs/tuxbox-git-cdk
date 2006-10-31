d0a9980a12860a8b0d199c7980aaedc8197d70e7
houdini <houdini>
2006-10-31 21:42:08 +0000

    activation of lzma-sqashfs (Thx JtG-Riker)

--- make/hostapps.mk
+++ make/hostapps.mk
@@ -25,11 +25,21 @@ $(hostprefix)/bin/mkcramfs: @DEPENDS_cramfs@
 	$(INSTALL) mkcramfs $@
 	rm -rf @DIR_cramfs@
 
+#######################
+#
+# mksquashfs with LZMA support
+#
 $(hostprefix)/bin/mksquashfs: @DEPENDS_squashfs@
-	@PREPARE_squashfs@
-	cd @DIR_squashfs@/squashfs-tools && \
-	$(MAKE) CC=$(CC) mksquashfs && \
-	$(INSTALL) mksquashfs $@
+	rm -rf @DIR_squashfs@
+	mkdir -p @DIR_squashfs@
+	cd @DIR_squashfs@ && \
+	bunzip2 -cd ../Archive/lzma442.tar.bz2 | TAPE=- tar -x && \
+	patch -p1 < ../Patches/lzma_zlib-stream.diff && \
+	gunzip -cd ../Archive/squashfs3.0.tar.gz | TAPE=- tar -x && \
+	patch -p0 < ../Patches/mksquashfs_lzma.diff
+	$(MAKE) -C @DIR_squashfs@/C/7zip/Compress/LZMA_Lib
+	$(MAKE) -C @DIR_squashfs@/squashfs3.0/squashfs-tools
+	$(INSTALL) -m755 @DIR_squashfs@/squashfs3.0/squashfs-tools/mksquashfs $@
 	rm -rf @DIR_squashfs@
 
 endif
