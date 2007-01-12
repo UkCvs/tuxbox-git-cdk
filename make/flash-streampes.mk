a999e445af919f23126ca41fa894bd0c70f356a8
yjogol <yjogol>
2007-01-12 16:13:06 +0000

    added udpstreamts for flash Used in yWeb 2.5 for upd-LiveView

--- make/flash-streampes.mk
+++ make/flash-streampes.mk
@@ -5,6 +5,7 @@ flash-streampes: $(appsdir)/dvb/tools/stream/streampes $(appsdir)/dvb/zapit/src/
 	$(INSTALL) $(appsdir)/dvb/tools/stream/streamsec $(flashprefix)/root/sbin
 	$(INSTALL) $(appsdir)/dvb/tools/stream/streamts $(flashprefix)/root/sbin
 	$(INSTALL) $(appsdir)/dvb/tools/stream/streampes $(flashprefix)/root/sbin
+	$(INSTALL) $(appsdir)/dvb/tools/stream/udpstreamts $(flashprefix)/root/sbin
 	$(INSTALL) $(appsdir)/dvb/zapit/src/.libs/udpstreampes $(flashprefix)/root/sbin
 	@FLASHROOTDIR_MODIFIED@
 	@TUXBOX_CUSTOMIZE@
