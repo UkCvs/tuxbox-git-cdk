9202168dabe692ff1aae8a7fbb26fa270faaad74
houdini <houdini>
2007-02-05 21:19:55 +0000

    update upnp, not tested with kernel 2.6

--- make/upnp.mk
+++ make/upnp.mk
@@ -1,4 +1,6 @@
-$(DEPDIR)/fuse: bootstrap @DEPENDS_fuse@ Patches/fuse.diff
+if KERNEL26
+# this contains the fuse library and tools and the fuse kernelmodule if kernel 2.6 is enabled
+$(DEPDIR)/fuse: bootstrap @DEPENDS_fuse@
 	@PREPARE_fuse@
 	cd @DIR_fuse@ && \
 	$(BUILDENV) \
@@ -9,8 +11,44 @@ $(DEPDIR)/fuse: bootstrap @DEPENDS_fuse@ Patches/fuse.diff
 	   --with-kernel=$(buildprefix)/$(KERNEL_DIR) \
 	   --prefix= && \
 	$(MAKE) all && \
+	@INSTALL_fuse@
+	@CLEANUP_fuse@
+	touch $@
+else
+# this contains the fuse library and tools if kernel 2.4 is enabled
+$(DEPDIR)/fuse: bootstrap $(DEPDIR)/fusekernel @DEPENDS_fuse@
+	@PREPARE_fuse@
+	cd @DIR_fuse@ && \
+	$(BUILDENV) \
+	CFLAGS="$(TARGET_CFLAGS) -I$(buildprefix)/linux/arch/ppc" \
+	./configure \
+	   --build=$(build) \
+	   --host=$(target) \
+	   --disable-kernel-module \
+	   --prefix= && \
+	$(MAKE) all && \
 	$(MAKE) install DESTDIR=$(targetprefix)
 	@CLEANUP_fuse@
+	touch $@
+endif
+
+# this contains the fuse kernelmodule for linux-2.4.x, fuse-2.6.x does only support kernel 2.6.x
+$(DEPDIR)/fusekernel: bootstrap @DEPENDS_fusekernel@
+	@PREPARE_fusekernel@
+	cd @DIR_fusekernel@ && \
+	$(BUILDENV) \
+	CFLAGS="$(TARGET_CFLAGS) -I$(buildprefix)/linux/arch/ppc" \
+	./configure \
+	   --build=$(build) \
+	   --host=$(target) \
+	   --with-kernel=$(buildprefix)/$(KERNEL_DIR) \
+	   --disable-lib \
+	   --disable-util \
+	   --disable-example \
+	   --prefix= && \
+	$(MAKE) all && \
+	@INSTALL_fusekernel@
+	@CLEANUP_fusekernel@
 	touch $@
 
 $(DEPDIR)/djmount: bootstrap fuse @DEPENDS_djmount@
@@ -30,7 +68,9 @@ if TARGETRULESET_FLASH
 
 flash-upnp: flash-fuse flash-djmount
 
-flash-fuse: bootstrap @DEPENDS_fuse@ Patches/fuse.diff
+if KERNEL26
+# this contains the fuse library and tools and the fuse kernelmodule if kernel 2.6 is enabled
+flash-fuse: bootstrap @DEPENDS_fuse@
 	@PREPARE_fuse@
 	cd @DIR_fuse@ && \
 	$(BUILDENV) \
@@ -41,8 +81,44 @@ flash-fuse: bootstrap @DEPENDS_fuse@ Patches/fuse.diff
 	   --with-kernel=$(buildprefix)/$(KERNEL_DIR) \
 	   --prefix= && \
 	$(MAKE) all && \
-	$(MAKE) install DESTDIR=$(flashprefix)/root
+	@INSTALL_fuse@
 	@CLEANUP_fuse@
+	@FLASHROOTDIR_MODIFIED@
+else
+# this contains the fuse library and tools if kernel 2.4 is enabled
+flash-fuse: bootstrap $(DEPDIR)/fusekernel flash-fusekernel @DEPENDS_fuse@
+	@PREPARE_fuse@
+	cd @DIR_fuse@ && \
+	$(BUILDENV) \
+	CFLAGS="$(TARGET_CFLAGS) -I$(buildprefix)/linux/arch/ppc" \
+	./configure \
+	   --build=$(build) \
+	   --host=$(target) \
+	   --disable-kernel-module \
+	   --prefix= && \
+	$(MAKE) all && \
+	$(MAKE) install DESTDIR=$(targetprefix)
+	@CLEANUP_fuse@
+	@FLASHROOTDIR_MODIFIED@
+endif
+
+# this contains the fuse kernelmodule for linux-2.4.x, fuse-2.6.x does only support kernel 2.6.x
+flash-fusekernel: bootstrap @DEPENDS_fusekernel@
+	@PREPARE_fusekernel@
+	cd @DIR_fusekernel@ && \
+	$(BUILDENV) \
+	CFLAGS="$(TARGET_CFLAGS) -I$(buildprefix)/linux/arch/ppc" \
+	./configure \
+	   --build=$(build) \
+	   --host=$(target) \
+	   --with-kernel=$(buildprefix)/$(KERNEL_DIR) \
+	   --disable-lib \
+	   --disable-util \
+	   --disable-example \
+	   --prefix= && \
+	$(MAKE) all && \
+	@INSTALL_fusekernel@
+	@CLEANUP_fusekernel@
 	@FLASHROOTDIR_MODIFIED@
 
 flash-djmount: bootstrap flash-fuse @DEPENDS_djmount@
