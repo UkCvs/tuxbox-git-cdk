717dc9c4a24eb80ed0bf33a7caecd5c0fcd3351a
houdini <houdini>
2007-10-03 10:52:19 +0000

    adapt to busybox  > 1.6.x

--- make/busybox.mk
+++ make/busybox.mk
@@ -7,12 +7,11 @@ endif
 
 $(DEPDIR)/busybox: bootstrap @DEPENDS_busybox@ Patches/busybox.config.m4 Patches/busybox.diff
 	@PREPARE_busybox@
-	m4 -Dyadd $(POSSIBLY_IDE) $(POSSIBLY_EXT3) Patches/busybox.config.m4 > @DIR_busybox@/.config
+	m4 -Dyadd $(POSSIBLY_IDE) $(POSSIBLY_EXT3) -DPREFIX="\"$(targetprefix)\"" Patches/busybox.config.m4 > @DIR_busybox@/.config
 	cd @DIR_busybox@ && \
-		$(MAKE) dep all \
+		$(MAKE) all install \
 			CROSS=$(target)- \
 			CFLAGS_EXTRA="$(TARGET_CFLAGS)" && \
-		@INSTALL_busybox@
 	@CLEANUP_busybox@
 	touch $@
 
@@ -21,12 +20,11 @@ if TARGETRULESET_FLASH
 
 flash-busybox: bootstrap $(flashprefix)/root @DEPENDS_busybox@ Patches/busybox.config.m4 Patches/busybox.diff
 	@PREPARE_busybox@
-	m4 -Dflash $(POSSIBLY_IDE) $(POSSIBLY_EXT3) Patches/busybox.config.m4 > @DIR_busybox@/.config
+	m4 -Dflash $(POSSIBLY_IDE) $(POSSIBLY_EXT3) -DPREFIX="\"$(flashprefix)/root\"" Patches/busybox.config.m4 > @DIR_busybox@/.config
 	cd @DIR_busybox@ && \
-		$(MAKE) dep all \
+		$(MAKE) all install \
 			CROSS=$(target)- \
 			CFLAGS_EXTRA="$(TARGET_CFLAGS)" && \
-		$(MAKE) install PREFIX=$(flashprefix)/root
 	@CLEANUP_busybox@
 	@FLASHROOTDIR_MODIFIED@
 
