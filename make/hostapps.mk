01aaf0e6190f6caa00b4e1f337c3952786558018
seife <seife>
2007-11-18 19:59:57 +0000

    For kernel 2.6, build mksquashfs without lzma support.

--- make/hostapps.mk
+++ make/hostapps.mk
@@ -33,11 +33,15 @@ $(hostprefix)/bin/mksquashfs: @DEPENDS_squashfs@
 	rm -rf @DIR_squashfs@
 	mkdir -p @DIR_squashfs@
 	cd @DIR_squashfs@ && \
+	gunzip -cd ../Archive/squashfs3.0.tar.gz | TAPE=- tar -x
+# TODO: implement LZMA in kernel, then remove this if-clause
+if !KERNEL26
+	cd @DIR_squashfs@ && \
 	bunzip2 -cd ../Archive/lzma442.tar.bz2 | TAPE=- tar -x && \
 	patch -p1 < ../Patches/lzma_zlib-stream.diff && \
-	gunzip -cd ../Archive/squashfs3.0.tar.gz | TAPE=- tar -x && \
 	patch -p0 < ../Patches/mksquashfs_lzma.diff
 	$(MAKE) -C @DIR_squashfs@/C/7zip/Compress/LZMA_Lib
+endif
 	$(MAKE) -C @DIR_squashfs@/squashfs3.0/squashfs-tools
 	$(INSTALL) -m755 @DIR_squashfs@/squashfs3.0/squashfs-tools/mksquashfs $@
 	rm -rf @DIR_squashfs@
