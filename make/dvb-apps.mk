e6b7797e7e6ab5495c94b4305a7e7fc12e717ec2
seife <seife>
2008-09-30 18:32:05 +0000

    Various updates for contrib apps: - Update bash 3.0 -> 3.2.39 - Update strace 4.5.12 -> 4.5.18, compiles also with uClibc - Update lsof 4.78 -> 4.80, URL of lsof-4.78 no longer existing - Update wget 1.10.2 -> 1.11.4 - Update ncftp 3.2.0 -> 3.2.2, URL of ncftp-3.2.0 no longer existing - Update gdb 6.6 -> 6.8 - Update joe 3.1 -> 3.5 - vls _does_ compile with GNU make on Debian Lenny

--- make/dvb-apps.mk
+++ make/dvb-apps.mk
@@ -3,11 +3,7 @@
 #  DVB apps
 #
 
-# Used version of VLS does not build with make 3.81, temporarily excluded
-# from dvb_apps
-
-# dvb_apps: dvbdate dvbstream dvbtext dvbtune vls
-dvb_apps: dvbdate dvbstream dvbtext dvbtune
+dvb_apps: dvbdate dvbstream dvbtext dvbtune vls
 
 $(DEPDIR)/dvbdate: bootstrap @DEPENDS_dvbdate@ Patches/dvbdate.diff
 	@PREPARE_dvbdate@
